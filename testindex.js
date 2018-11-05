a7ba240d6ee19ba6db342f030fc418d5544580af
boppe123 <44640763+boppe123@users.noreply.github.com>
2018-11-05 14:41:37 +0100

    Add files via upload

---
+++ testindex.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const https = require('https');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const restService = express();
+
+const host = 'api.thingspeak.com';
+
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+restService.use(bodyParser.json());
+
+restService.post("/webhooker", function(req, res) {
+
+ let unit = req.body.queryResult.parameters['unit']; // take out the Unit, lamp e.g.
+ let state = req.body.queryResult.parameters['state']; // take out the the state, on or off
+ let cmd = req.body.queryResult.parameters['cmd'];
+ 
+ if (unit == 'lamp' && state == 'on' && cmd == 'turn'){
+	 callThingApiON().then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': 'something is wrong' });
+  });
+ }else{
+	callThingApiOFF().then((output) => {
+    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+  }).catch(() => {
+    res.json({ 'fulfillmentText': 'something is wrong' });
+  }); 
+ }
+});
+
+
+
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
+
+
+
+function callThingApiON () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+	
+    https.get('https://api.thingspeak.com/update?api_key=TOVVVTT2PA4I9HB5&field1=1', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        //let response = JSON.parse(body);
+        //let last = response['field1'];
+        // Create response
+        let output = 'The state is now on';
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
+
+function callThingApiOFF () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=0';
+    // Make the HTTP request
+	
+    https.get('https://api.thingspeak.com/update?api_key=TOVVVTT2PA4I9HB5&field1=0', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        //let response = JSON.parse(body);
+        //let last = response['field1'];
+        // Create response
+        let output = 'The state is now off';
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
+
+
